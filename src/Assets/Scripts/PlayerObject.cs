791848027e9715f2db0a393a5739aad4642f19d2
Dustin Heffron <heffjoker>
2015-01-24 15:02:40 -0500

    Added spawners and win condition.

--- src/Assets/Scripts/PlayerObject.cs
+++ src/Assets/Scripts/PlayerObject.cs
@@ -5,12 +5,18 @@ using InControl;
 public class PlayerObject : MonoBehaviour {
 
 	public float knockbackForce = 20;
+	public float stunTimeout = 2f;
 	public Transform MountPos;
 	public SlapObject slapObj;
 	public string playerName = string.Empty;
 
 	private MoveObj mover;
 	private GoalObject goalTrans;
+
+	public bool HasGoalObj
+	{
+		get { return goalTrans != null; }
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -52,11 +58,31 @@ public class PlayerObject : MonoBehaviour {
 		transform.localScale = new Vector3(dirVal, transform.localScale.y, transform.localScale.z);
 	}
 
-	void HandleKnockback(Vector3 dir)
+	public void HandleKnockback(Vector3 dir)
 	{
 		dir.Normalize();
 		rigidbody2D.velocity = Vector2.zero;
 		rigidbody2D.AddForce(dir * knockbackForce);
+	}
+
+	public void DisableControl()
+	{
+		mover.enabled = false;
+	}
+
+	void EnableControl()
+	{
+		mover.enabled = true;
+	}
+
+	public void DisableCollision()
+	{
+		collider2D.enabled = false;
+	}
+
+	public void EnableCollision()
+	{
+		collider2D.enabled = true;
 	}
 
 	void OnTriggerEnter2D(Collider2D collider)
@@ -79,7 +105,12 @@ public class PlayerObject : MonoBehaviour {
 				goalTrans = null;
 			}
 
-			HandleKnockback(transform.position - collider.transform.position);
+			if(mover.enabled)
+			{
+				DisableControl();
+				Invoke("EnableControl", stunTimeout);
+				HandleKnockback(transform.position - collider.transform.position);
+			}
 		}
 	}
 }
