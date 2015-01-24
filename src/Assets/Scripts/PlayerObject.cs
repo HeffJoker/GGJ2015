786c0e7e30b6d9eeaf326072f44c7dad2aeb20ea
Dustin Heffron <heffjoker>
2015-01-23 21:27:42 -0500

    Initial addition of code as well as code to "mount" the goal obj to the player.

---
+++ src/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerObject : MonoBehaviour {
+
+	public Transform MountPos;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.CompareTag("GoalObj"))
+		{
+			if(MountPos == null)
+			{
+				collider.transform.position = transform.position;
+				collider.transform.parent = transform;
+			}
+			else
+			{
+				collider.transform.position = MountPos.position;
+				collider.transform.parent = MountPos;
+			}
+		}
+	}
+}
