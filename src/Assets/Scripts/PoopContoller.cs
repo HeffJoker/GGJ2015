deea490dbc04e0640d4da1ffff410c57ba513547
MattBridges <Matthew.Bridges1@gmail.com>
2015-01-24 20:01:19 -0500

    Added scripts and prefab for poop bars...Matt

---
+++ src/Assets/Scripts/PoopContoller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoopContoller : MonoBehaviour {
+	public float curPoop;
+	public float poopInc = .1f;
+	public bool gameOver=false;
+	// Use this for initialization
+	void Start () {
+		curPoop = 0;
+		StartCoroutine ("IncreasePoop");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	IEnumerator IncreasePoop()
+	{
+		while(!gameOver)
+		{
+			yield return new WaitForSeconds (.1f);
+			curPoop+=poopInc;
+		}
+	}
+
+	public float GetPoop()
+	{
+		return curPoop;
+	}
+}
